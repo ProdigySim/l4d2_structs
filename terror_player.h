d8cd4c63201459470938aba057e626db83a631db
Michael Busby <mibusby@gmail.com>
2012-03-20 00:22:30 -0500

    Refactored vtable structs so it's much easier to read in IDA. Added basic CTerrorPlayer structures.

--- terror_player.h
+++ terror_player.h
@@ -2,12 +2,79 @@
 #define _INCLUDE_TERROR_PLAYER_H_
 
 #include "terror_player_vtables.h"
+#include "timers.h"
+
+struct CBaseEntity_data
+{
+	char unknown[1068];
+};
+
+struct CBaseAnimating_data
+{
+	char unknown[4024]; // 1072
+};
+
+struct CBaseFlex_data
+{
+	char unknown[996]; // 5096
+};
+
+struct CBaseCombatCharacter_data
+{
+	char unknown[644]; // 6092
+};
+
+struct CBasePlayer_data
+{
+	char unknown[2344]; // 6736
+};
+
+
+struct CAI_ExpresserHost_data
+{
+	CAI_ExpresserSink_vtable * vptr; // 9080
+};
+
+// From game/server/basemultiplayerplayer.h
+struct CBaseMultiplayerPlayer_data
+{
+	int m_iIgnoreGlobalChat; // 9084
+	float m_flAreaCaptureScoreAccumulator; // 9088
+	float m_flCapPointScoreRate; // 9092
+	int	m_iCurrentConcept; // 9096
+	void *m_pExpresser; // 9100 CMultiplayer_Expresser *
+	float m_flConnectionTime; // 9104
+	float m_flLastForcedChangeTeamTime; // 9108
+	int m_iBalanceScore; // 9112
+	void *m_pAchievementKV; // 9116 KeyValues *
+};
+
+struct CCSPlayer_data
+{
+	void * m_pHintMessageQueue; // 9120 CHintMessageQueue
+	char unknown[1708]; // 9124
+	CountdownTimer UnknownTimer; // 10832
+};
+
+
+struct CTerrorPlayer_data
+{
+	char unknown[5548]; // 10844
+};
 
 // size 16392
 struct CTerrorPlayer
 {
-	CTerrorPlayer_vtable * vptr;
-	char unknown[16388]; // 0x04 TODO: other classes :P
+	CTerrorPlayer_vtable * vptr; // 0
+	CBaseEntity_data CBaseEntity; // 4
+	CBaseAnimating_data CBaseAnimating; // 1072
+	CBaseFlex_data CBaseFlex; // 5096
+	CBaseCombatCharacter_data CBaseCombatCharacter; // 6092
+	CBasePlayer_data CBasePlayer; // 6736
+	CAI_ExpresserHost_data CAI_ExpresserHost; // 9080
+	CBaseMultiplayerPlayer_data CBaseMultiplayerPlayer; // 9084
+	CCSPlayer_data CCSPlayer; // 9120
+	CTerrorPlayer_data CTerrorPlayer; // 10844
 };
 
 #endif // _INCLUDE_TERROR_PLAYER_H_
